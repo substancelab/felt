7c71edfa0258ca0132557a0925670fc032f0e231
Jakob Skjerning <jakob@mentalized.net>
2023-01-21 14:58:54 +0100

    Generate initial gem
        bundle gem --coc --mit --test=minitest --ci=github --linter=standard
    felt

---
+++ lib/felt.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "felt/version"
+
+module Felt
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/felt/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Felt
+  VERSION = "0.1.0"
+end
