a5a59aebcc3750ea487a44ef51e72273ed222776
Jakob Skjerning <jakob@mentalized.net>
2023-01-22 17:01:39 +0100

    Create previews for TextComponent

--- previews/input_group/text_component_preview.rb
+++ previews/input_group/text_component_preview.rb
@@ -5,14 +5,37 @@ module InputGroup
     include ActiveModel::API
 
     attr_accessor :title
-    attr_accessor :description
   end
 
   class TextComponentPreview < ViewComponent::Preview
     def default
       model = Game.new
       model.title = "Dig Dug"
-      model.description = "Diggin' game"
+      form = build_form(model)
+      render(Felt::InputGroup::TextComponent.new(form: form, attribute: :title))
+    end
+
+    def with_label
+      model = Game.new
+      form = build_form(model)
+      render(Felt::InputGroup::TextComponent.new(form: form, attribute: :title, label: "Game Title"))
+    end
+
+    def with_hint
+      model = Game.new
+      form = build_form(model)
+      render(Felt::InputGroup::TextComponent.new(form: form, attribute: :title, hint: "Enter the title of the game"))
+    end
+
+    def with_help
+      model = Game.new
+      form = build_form(model)
+      render(Felt::InputGroup::TextComponent.new(form: form, attribute: :title, help: "Games can have different titles in different regions."))
+    end
+
+    def with_errors
+      model = Game.new
+      model.errors.add(:title, :invalid)
       form = build_form(model)
       render(Felt::InputGroup::TextComponent.new(form: form, attribute: :title))
     end
